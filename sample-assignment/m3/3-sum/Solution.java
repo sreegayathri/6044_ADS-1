f2a839101b93e2069a9030a803c23c0b9033be3f
sreegayathri <sree0291@msitprogram.net>
2018-09-26 19:46:24 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- sample-assignment/m3/3-sum/Solution.java
+++ sample-assignment/m3/3-sum/Solution.java
@@ -1,47 +1,47 @@
 import java.util.Scanner;
 import java.util.Arrays;
-/**
+/**.
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
+    }
+    /**.
+     * { function_description }.
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
 // Solution nop = new Solution();
-		Scanner scan = new Scanner(System.in);
-		/*		int line = scan.nextLine();*/
-		int n = scan.nextInt();
-		int[] ThreeSum = new int[n];
-		for (int i = 0; i < ThreeSum.length; i++) {
+        Scanner scan = new Scanner(System.in);
+        /*      int line = scan.nextLine();*/
+        int n = scan.nextInt();
+        int[] threeSum = new int[n];
+        for (int i = 0; i < threeSum.length; i++) {
 //System.out.println(" number");
-			ThreeSum[i] = scan.nextInt();
-		}// n
-		int l, r, count = 0;
-		Arrays.sort(ThreeSum);//nlogn
-		for (int i = 0; i < n - 2; i++) {
-			l = i + 1;
-			r = n - 1; // index of the last element
-			while (l < r) {
-				if (ThreeSum[i] + ThreeSum[l] + ThreeSum[r] == 0) {
-					count++;
-					l++;
-					r--;
-				} else if (ThreeSum[i] + ThreeSum[l] + ThreeSum[r] < 0) {
-					l++;
-				} else {
-					r--;
-				}
-			}
-		}
-		System.out.println(count);
+            threeSum[i] = scan.nextInt();
+        }// n
+        int l, r, count = 0;
+        Arrays.sort(threeSum);//nlogn
+        for (int i = 0; i < n - 2; i++) {
+            l = i + 1;
+            r = n - 1; // index of the last element
+            while (l < r) {
+                if (threeSum[i] + threeSum[l] + threeSum[r] == 0) {
+                    count++;
+                    l++;
+                    r--;
+                } else if (threeSum[i] + threeSum[l] + threeSum[r] < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        System.out.println(count);
 
-	}
+    }
 }
