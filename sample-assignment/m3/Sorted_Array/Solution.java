2b6fc0d546f26c0aca9e3188f7c4d8f71da1d972
sreegayathri <sree0291@msitprogram.net>
2018-09-26 23:28:46 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- sample-assignment/m3/Sorted_Array/Solution.java
+++ sample-assignment/m3/Sorted_Array/Solution.java
@@ -1,23 +1,62 @@
 import java.util.Scanner;
+/**.
+ * { item_description }.
+ */
 public final class Solution {
+	/**.
+	 * Constructs the object.
+	 */
 	Solution() {
 
 	}
-	public static void main(String[] args) {
+	/**.
+	 * { function_description }.
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
         /*      int line = scan.nextLine();*/
         int sizefirst = scan.nextInt();
         int sizetwo = scan.nextInt();
         int[] firstArray = new int[sizefirst];
         int[] twoArray = new int[sizetwo];
+        String[] arr1 = scan.nextLine().split(",");
+        String[] arr2 = scan.nextLine().split(",");
         scan.nextLine();
         for (int i = 0; i < firstArray.length; i++) {
-//System.out.println(" number");
-            firstArray[i] = scan.nextInt();
+		//System.out.println(" number");
+            firstArray[i] = Integer.parseInt(arr1[i]);
         } // n
-        for (int i = 0; i < twoArray.length; i++) {
-//System.out.println(" number");
-            twoArray[i] = scan.nextInt();
+        for (int j = 0; j < twoArray.length; j++) {
+		//System.out.println(" number");
+            twoArray[j] = Integer.parseInt(arr2[j]);
         } // n
+
+        int size = sizefirst + sizetwo;
+        int[] finalarray = new int[size];
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while(i < firstArray.length && j < twoArray.length) {
+	        if (firstArray[i] < twoArray[j]) {
+	        	finalarray[index] = firstArray[i];
+	        	i++;
+	        } else {
+	        	finalarray[index] = twoArray[j];
+	        	j++;
+	        }
+        }
+        while(i < firstArray.length) {
+	        if (firstArray[i] < twoArray[j]) {
+	        	finalarray[index] = firstArray[i];
+	        	i++;
+	        } 
+        }
+	    while (j < twoArray.length) {
+	    	if (firstArray[i] > twoArray[j]) {
+	    		finalarray[index] = twoArray[j];
+	    		j++;
+	    	}
+	    }
 	}
 }
