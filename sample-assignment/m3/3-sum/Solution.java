d6954e34012c8a4c818ea0431965ab3bb7a90094
sreegayathri <sree0291@msitprogram.net>
2018-09-26 19:46:58 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- sample-assignment/m3/3-sum/Solution.java
+++ sample-assignment/m3/3-sum/Solution.java
@@ -23,9 +23,9 @@ public final class Solution {
         for (int i = 0; i < threeSum.length; i++) {
 //System.out.println(" number");
             threeSum[i] = scan.nextInt();
-        }// n
+        } // n
         int l, r, count = 0;
-        Arrays.sort(threeSum);//nlogn
+        Arrays.sort(threeSum); //nlogn
         for (int i = 0; i < n - 2; i++) {
             l = i + 1;
             r = n - 1; // index of the last element
