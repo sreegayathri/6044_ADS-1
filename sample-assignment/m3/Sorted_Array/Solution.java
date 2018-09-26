d0758a4c92b034362d666fe115dd4cc21e55d696
sreegayathri <sree0291@msitprogram.net>
2018-09-26 23:52:39 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- sample-assignment/m3/Sorted_Array/Solution.java
+++ sample-assignment/m3/Sorted_Array/Solution.java
@@ -61,6 +61,6 @@ public final class Solution {
 	    		j++;
 	    	// }
 	    }
-	    System.out.println(Arrays.toString(finalarray));
+	    System.out.println(Arrays.toString(finalarray).replaceAll("[//]//[ ]",""));
 	}
 }
