3a5cb0533fb09fd6e7670892874687e6aa06c257
sreegayathri <sree0291@msitprogram.net>
2018-09-26 23:56:15 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- sample-assignment/m3/Sorted_Array/Solution.java
+++ sample-assignment/m3/Sorted_Array/Solution.java
@@ -3,18 +3,18 @@ import java.util.*;
  * { item_description }.
  */
 public final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	Solution() {
+    /**.
+     * Constructs the object.
+     */
+    Solution() {
 
-	}
-	/**.
-	 * { function_description }.
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
+    }
+    /**.
+     * { function_description }.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
         /*      int line = scan.nextLine();*/
         int sizefirst = scan.nextInt();
         int sizetwo = scan.nextInt();
@@ -26,11 +26,11 @@ public final class Solution {
         /*System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(arr2));*/
         for (int i = 0; i < firstArray.length; i++) {
-		//System.out.println(" number");
+        //System.out.println(" number");
             firstArray[i] = Integer.parseInt(arr1[i]);
         } // n
         for (int j = 0; j < twoArray.length; j++) {
-		//System.out.println(" number");
+        //System.out.println(" number");
             twoArray[j] = Integer.parseInt(arr2[j]);
         } // n
 
@@ -39,28 +39,29 @@ public final class Solution {
         int i = 0;
         int j = 0;
         int index = 0;
-        while(i < firstArray.length && j < twoArray.length) {
-	        if (firstArray[i] < twoArray[j]) {
-	        	finalarray[index++] = firstArray[i];
-	        	i++;
-	        } else {
-	        	finalarray[index++] = twoArray[j];
-	        	j++;
-	        }
+        while (i < firstArray.length && j < twoArray.length) {
+            if (firstArray[i] < twoArray[j]) {
+                finalarray[index++] = firstArray[i];
+                i++;
+            } else {
+                finalarray[index++] = twoArray[j];
+                j++;
+            }
 
         }
-        while(i < firstArray.length) {
-	        // if (fibrstArray[i] < twoArray[j]) {
-	        	finalarray[index++] = firstArray[i];
-	        	i++;
-	        // } 
+        while (i < firstArray.length) {
+            // if (fibrstArray[i] < twoArray[j]) {
+                finalarray[index++] = firstArray[i];
+                i++;
+            // }
         }
-	    while (j < twoArray.length) {
-	    	// if (firstArray[i] > twoArray[j]) {
-	    		finalarray[index++] = twoArray[j];
-	    		j++;
-	    	// }
-	    }
-	    System.out.println(Arrays.toString(finalarray).replaceAll("[\\]\\[ ]",""));
-	}
+        while (j < twoArray.length) {
+            // if (firstArray[i] > twoArray[j]) {
+                finalarray[index++] = twoArray[j];
+                j++;
+            // }
+        }
+        System.out.println(Arrays.toString(finalarray).
+                                replaceAll("[\\]\\[ ]", ""));
+    }
 }
