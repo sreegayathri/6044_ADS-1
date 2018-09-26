a5db70de0af12fba867237fabecd698058458c86
sreegayathri <sree0291@msitprogram.net>
2018-09-26 19:40:49 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- sample-assignment/m3/3-sum/Solution.java
+++ sample-assignment/m3/3-sum/Solution.java
@@ -15,11 +15,7 @@ class Solution {
 		int l, r, count = 0;
 		Arrays.sort(ThreeSum);//nlogn
 		for (int i = 0; i < n - 2; i++) {
-
-// To find the other two elements, start two index variables
-// from two corners of the array and move them toward each
-// other
-			l = i + 1; // index of the first element in the remaining elements
+			l = i + 1;
 			r = n - 1; // index of the last element
 			while (l < r) {
 				if (ThreeSum[i] + ThreeSum[l] + ThreeSum[r] == 0) {
@@ -28,7 +24,7 @@ class Solution {
 					r--;
 				} else if (ThreeSum[i] + ThreeSum[l] + ThreeSum[r] < 0) {
 					l++;
-				} else { // A[i] + A[l] + A[r] > sum
+				} else {
 					r--;
 				}
 			}
