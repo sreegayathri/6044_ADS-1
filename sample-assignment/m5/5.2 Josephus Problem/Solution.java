d447fff6262d01d0276cd1e905f2f89b63e37d74
sreegayathri <sree0291@msitprogram.net>
2018-09-30 15:37:47 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- sample-assignment/m5/5.2 Josephus Problem/Solution.java
+++ sample-assignment/m5/5.2 Josephus Problem/Solution.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
+
+/**.
+ * { item_description }
+ */
 public final class Solution {
+    /**.
+     * Constructs the object.
+     */
     private Solution() {
 
     }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
