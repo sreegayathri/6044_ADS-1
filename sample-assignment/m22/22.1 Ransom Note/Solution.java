de15de60225fa98f6250722499bacd5b26b9422c
sreegayathri <sree0291@msitprogram.net>
2018-10-26 00:50:06 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- sample-assignment/m22/22.1 Ransom Note/Solution.java
+++ sample-assignment/m22/22.1 Ransom Note/Solution.java
@@ -1,34 +1,45 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
-	Solution() { }
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		sc.nextLine();
-		String[] magazine = sc.nextLine().split(" ");
-		SeparateChainingHashST<String, Integer> h = new SeparateChainingHashST<>();
-		for (int i = 0; i < m; i++) {
-			if (h.contains(magazine[i])) {
-				h.put(magazine[i], h.get(magazine[i]) + 1);
-			} else {
-				h.put(magazine[i], 1);
-			}
-		}
-		String[] note = sc.nextLine().split(" ");
-		for (int j = 0; j < n; j++) {
-			if (h.contains(note[j])) {
-				if (h.get(note[j]) == 0) {
-					System.out.println("No");
-					return;
-				} else {
-					h.put(note[j], h.get(note[j]) - 1);
-				}
-			} else {
-				System.out.println("No");
-				return;
-			}
-		}
-		System.out.println("Yes");
-	}
+    /**
+     * Constructs the object.
+     */
+    Solution() { }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] magazine = sc.nextLine().split(" ");
+        SeparateChainingHashST<String, Integer> h = new SeparateChainingHashST<>();
+        for (int i = 0; i < m; i++) {
+            if (h.contains(magazine[i])) {
+                h.put(magazine[i], h.get(magazine[i]) + 1);
+            } else {
+                h.put(magazine[i], 1);
+            }
+        }
+        String[] note = sc.nextLine().split(" ");
+        for (int j = 0; j < n; j++) {
+            if (h.contains(note[j])) {
+                if (h.get(note[j]) == 0) {
+                    System.out.println("No");
+                    return;
+                } else {
+                    h.put(note[j], h.get(note[j]) - 1);
+                }
+            } else {
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
 }
