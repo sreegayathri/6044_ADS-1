21ed0d0de19ad8d0a8cd7b610c77fb3e147f6786
sreegayathri <sree0291@msitprogram.net>
2018-10-30 22:17:31 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- sample-assignment/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/LinkedList.java
+++ sample-assignment/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/LinkedList.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**.
  * List of linkeds.
  */
@@ -31,6 +30,8 @@ public class LinkedList {
     }
     /**.
      * insertAt.
+     * time complexity of this method is 1
+     * because the function is called only at once.
      * @param      position  The position
      * @param      data      The data
      * @return     { description_of_the_return_value }
@@ -47,6 +48,8 @@ public class LinkedList {
     }
     /**
      * insertHelper.
+ 	 * The time complexity of this method is N because it is
+     * recursive and called N times.
      * @param      head  The head
      * @param      count   The count
      * @param      element   The element
@@ -59,13 +62,15 @@ public class LinkedList {
     }
     /**
      * reverse.
+     * The time complexity is constant because the method is called only once
      */
     public void reverse() {
         head = reverseHelper(head);
     }
     /**
-     * reverseHelper.
-     *
+    * reverseHelper.
+    * { this is overloaded method of the above reverse method }
+	* time complexity is N because it is an recursive method that can be called N times.
      * @param      head  The head
      *
      * @return     { description_of_the_return_value }
@@ -82,7 +87,7 @@ public class LinkedList {
     /**
      * print.
      *
-     * @return     { description_of_the_return_value }
+     * @return String representation of the object.
      */
     public String print() {
         Node thead = head;
@@ -94,101 +99,4 @@ public class LinkedList {
 
         return s.substring(0 , s.length() - 2);
     }
-=======
-/**.
- * List of linkeds.
- */
-public class LinkedList {
-    /**.
-    * head;
-    */
-    private Node head;
-    /**.
-     * Class for node.
-     */
-    private class Node {
-        /**.
-         * data.
-         */
-        private int data;
-        /**.
-         * next.
-         */
-        private Node next;
-        /**.
-         * Constructs the object.
-         * @param      data  The data
-         * @param      next  The next
-         */
-        Node(int data, Node next) {
-            this.data = data;
-            this.next = next;
-        }
-    }
-    /**.
-     * insertAt.
-     * @param      position  The position
-     * @param      data      The data
-     * @return     { description_of_the_return_value }
-     */
-    public boolean insertAt(int position, int data) {
-        boolean flag = false;
-        try {
-            head = insertHelper(head, position , data);
-        } catch (Exception e) {
-            System.out.println("Can't insert at this position.");
-            flag = true;
-        }
-        return flag;
-    }
-    /**
-     * insertHelper.
-     * @param      head  The head
-     * @param      count   The count
-     * @param      element   The element
-     * @return     { description_of_the_return_value }
-     */
-    public Node insertHelper(Node head, int count, int element) {
-        if (count == 0) return new Node(element, head);
-        head.next = insertHelper(head.next, count - 1, element);
-        return head;
-    }
-    /**
-     * reverse.
-     */
-    public void reverse() {
-        head = reverseHelper(head);
-    }
-    /**
-     * reverseHelper.
-     *
-     * @param      head  The head
-     *
-     * @return     { description_of_the_return_value }
-     */
-    Node reverseHelper(Node head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        Node nhead = reverseHelper(head.next);
-        head.next.next = head;
-        head.next = null;
-        return nhead;
-    }
-    /**
-     * print.
-     *
-     * @return     { description_of_the_return_value }
-     */
-    public String print() {
-        Node thead = head;
-        String s  = "";
-        while (thead != null) {
-            s += thead.data + ", ";
-            thead = thead.next;
-        }
-
-        return s.substring(0 , s.length() - 2);
-    }
->>>>>>> aad999d34dd3fdaa0f6842661f0b26239fbae5fa
 }
