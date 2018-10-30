49edbe7144c2dea71b5b1cb9afdac4b14e7190cb
sreegayathri <sree0291@msitprogram.net>
2018-10-30 19:38:45 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- sample-assignment/m14/14.2 student reservation system/14.2 student reservation system/Solution.java
+++ sample-assignment/m14/14.2 student reservation system/14.2 student reservation system/Solution.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 /**
  * Class for solution.
@@ -38,44 +37,3 @@ final class Solution {
         h.board(temp);
     }
 }
-=======
-import java.util.Scanner;
-/**
- * Class for solution.
- */
-final class Solution {
-    /**
-     * Constructs the object.
-     */
-    private Solution() {
-        //function.
-    }
-    /**
-     * main.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        int o = Integer.parseInt(sc.nextLine());
-        int p = Integer.parseInt(sc.nextLine());
-        int q = Integer.parseInt(sc.nextLine());
-        int r = Integer.parseInt(sc.nextLine());
-        int s = Integer.parseInt(sc.nextLine());
-        Student[] list = new Student[n];
-        int temp = n;
-        int i = 0;
-        while (n > 0) {
-            String[] tokens = sc.next().split(",");
-            list[i++] = new Student(tokens[0], tokens[1], Integer.parseInt(tokens[2]),
-            Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]),
-            Integer.parseInt(tokens[5]), tokens[6]);
-            n--;
-        }
-        Heap h = new Heap(o, p, q, r, s, list, i);
-        list = h.sort();
-        h.board(temp);
-    }
-}
->>>>>>> aad999d34dd3fdaa0f6842661f0b26239fbae5fa
